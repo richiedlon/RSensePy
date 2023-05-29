abd67a18b3c446d42c002969c8e3888456c3be5a
richiedlon <rde.ekanayakag@gmail.com>
2023-05-29 23:24:19 +0200

    Updated cloud mask
    Created two separate cloud mask functions for bounding box and shapefile

--- cloudMask_clip.py
+++ cloudMask_clip.py
@@ -5,19 +5,33 @@ import rasterio
 from clip_withBB_function import clipRasterBB
 from clip_withSHP_function import clipRasterSHP
 from writeRasterFunction import writeRaster
-
-os.chdir('C://Studies//Copernicus Program//1_Semester 2//Software development practice//Final project')
-locationRasterQA="LC08_L2SP_191027_20220720_20220726_02_T1/LC08_L2SP_191027_20220720_20220726_02_T1_QA_PIXEL.TIF"
-shpLocation= "AOI/AOI.shp"
-outputLocation1 = "output/CloudMaskCliped.tif"
-outputLocation2 = "output/CloudMaskFull.tif"
-
-def cloud_mask_landsat8_clip(image_path, locationSHP):
-    minx, miny = 13.490206, 48.3355
-    maxx, maxy = 14.076421, 48.007881
-
+def cloud_mask_landsat8_clip_shp(image_path, locationSHP):
     resultTest=clipRasterSHP(image_path,locationSHP)
-    #resultTest=clipRasterBB(locationRaster,minx,miny,maxx,maxy)
+    qa_bandTest = resultTest[0]
+    #Extract cloud and cloud shadow bits
+    cloud_bit = 3
+    cloud_shadow_bit = 4
+
+    # Create cloud mask based on confidence thresholds
+    cloud_mask = np.bitwise_and(np.right_shift(qa_bandTest, cloud_bit), 1)
+    cloud_shadow_mask = np.bitwise_and(np.right_shift(qa_bandTest, cloud_shadow_bit), 1)
+    cloud_mask = np.where(qa_bandTest >= (1 << cloud_bit), cloud_mask, 0)
+    cloud_shadow_mask = np.where(qa_bandTest >= (1 << cloud_shadow_bit), cloud_shadow_mask, 0)
+    
+    # Combine cloud and cloud shadow masks
+    
+    combined_mask = np.logical_or(cloud_mask, cloud_shadow_mask)
+    combined_mask =  np.where((combined_mask==0)|(combined_mask==1), combined_mask^1,combined_mask) # Convert zeors to one and ones to zero
+    combined_mask = np.where(combined_mask==0, np.nan, combined_mask) # Assign null values to zeros
+    return combined_mask,resultTest[1]
+
+def cloud_mask_landsat8_clip(image_path, bbcoord):
+    # minx, miny = 13.490206, 48.3355
+    # maxx, maxy = 14.076421, 48.007881
+    #print(bbcoord[0])
+    #resultTest=clipRasterSHP(image_path,locationSHP)
+    resultTest=clipRasterBB(image_path,bbcoord)
+
 
     qa_bandTest = resultTest[0]
     #Extract cloud and cloud shadow bits
@@ -58,25 +72,15 @@ def cloud_mask_landsat8_clip(image_path, locationSHP):
     # Combine cloud and cloud shadow masks
     
     combined_mask = np.logical_or(cloud_mask, cloud_shadow_mask)
-    print("Clip cloud mask shape - before squeeze \n")
-    # print(combined_mask.shape)
-    print(resultTest[1])
-    print(combined_mask.shape)
-    writeRaster(combined_mask,resultTest[1],outputLocation1)  
-        
+    # print(combined_mask.shape)      
     #Plot the cloud mask
     # Need to squeeze before plotting
 
-    plt.figure(figsize=(10, 10))
-    plt.imshow(combined_mask.squeeze(), cmap='gray')
-    plt.title('Cloud Mask')
-    plt.colorbar()
-    plt.show()    
+    # plt.figure(figsize=(10, 10))
+    # plt.imshow(combined_mask.squeeze(), cmap='gray')
+    # plt.title('Cloud Mask')
+    # plt.colorbar()
+    # plt.show()
+    combined_mask =  np.where((combined_mask==0)|(combined_mask==1), combined_mask^1,combined_mask) # Convert zeors to one and ones to zero
+    combined_mask = np.where(combined_mask==0, np.nan, combined_mask) # Assign null values to zeros
     return combined_mask,resultTest[1]
-
-combined = cloud_mask_landsat8_clip(locationRasterQA, shpLocation)
-
-# print(combined.ndim)
-# print(combined.shape)
-# print(combined.size)
-# print(combined.itemsize)
