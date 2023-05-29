2d6adc1069147f4a14af995d1166e11e7bbf3d91
richiedlon <rde.ekanayakag@gmail.com>
2023-05-29 23:26:16 +0200

    Update clip_withBB_function.py
    Deleted testing statements

--- clip_withBB_function.py
+++ clip_withBB_function.py
@@ -8,7 +8,6 @@ from rasterio.mask import mask
 from shapely.geometry import box
 import geopandas as gpd
 from fiona.crs import from_epsg
-# import pycrs
 from rasterio.crs import CRS
 
 from writeRasterFunction import writeRaster
@@ -20,10 +19,10 @@ def getFeatures(gdf):
     import json
     return [json.loads(gdf.to_json())['features'][0]['geometry']]
 
-def clipRasterBB(locationRaster,minx,miny,maxx,maxy): #Location - original raster file location and bounding box coordinates
+def clipRasterBB(locationRaster,bbox): #Location - original raster file location and bounding box coordinates bbbox = [minx,miny,maxx,maxy]
 	Raster =rasterio.open(locationRaster) # Open raster file
 	CRSraster = Raster.crs # Get raster file coordinates
-	bbox = box(minx, miny, maxx, maxy) # Define bounding box
+	bbox = box(bbox[0], bbox[1], bbox[2], bbox[3]) # Define bounding box
 	geo = gpd.GeoDataFrame({'geometry': bbox}, index=[0], crs=from_epsg(4326)) # Define bouding box coordinate system
 	geo = geo.to_crs(CRSraster) # Project bounding box coordinates to raster coordinate system
 	coords = getFeatures(geo) # Get bounding box coordinates in raster coordinate system
@@ -34,18 +33,7 @@ def clipRasterBB(locationRaster,minx,miny,maxx,maxy): #Location - original raste
 				"height": out_img.shape[1], 
 				"width": out_img.shape[2], 
 				"transform": out_transform,
+				"count":1,
+				"dtype":'float32',
 				"crs": CRS.from_epsg(epsg_code)}) # Update meta data for the clipped raster
 	return out_img,out_meta,epsg_code  # Return clipped image as a numpy, meta data and epsg_code
-
-
-# minx, miny = 13.490206, 48.3355
-# maxx, maxy = 14.076421, 48.007881
-
-
-# rasterLocation = "LC08_L2SP_191027_20220720_20220726_02_T1/LC08_L2SP_191027_20220720_20220726_02_T1_ST_B10.TIF"
-# outputLocation = "output/MaskedTest.tif"
-# result=clipRasterBB(rasterLocation,minx,miny,maxx,maxy)
-
-# writeRaster(result[0],result[1],outputLocation)
-
-# print ("EPSG code"+str(result[2]))
