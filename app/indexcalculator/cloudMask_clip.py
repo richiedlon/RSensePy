059cb5ef3d21c556049349bd33651ba7007702df
richiedlon <rde.ekanayakag@gmail.com>
2023-07-30 21:57:27 +0200

    Added comments for the files

--- app/indexcalculator/cloudMask_clip.py
+++ app/indexcalculator/cloudMask_clip.py
@@ -26,33 +26,11 @@ def cloud_mask_landsat8_clip_shp(image_path, locationSHP):
     return combined_mask,resultTest[1]
 
 def cloud_mask_landsat8_clip(image_path, bbcoord):
-    # minx, miny = 13.490206, 48.3355
-    # maxx, maxy = 14.076421, 48.007881
-    #print(bbcoord[0])
-    #resultTest=clipRasterSHP(image_path,locationSHP)
-    resultTest=clipRasterBB(image_path,bbcoord)
-
-
+    resultTest=clipRasterBB(image_path,bbcoord)  # Call clip function to clip the QA band.
     qa_bandTest = resultTest[0]
     #Extract cloud and cloud shadow bits
     cloud_bit = 3
     cloud_shadow_bit = 4
-
-
-
-    # valuetoTest = 21824
-    # print("cloudValue "+ str(valuetoTest))
-    # print("binary of valuetoTest ="+str(bin(valuetoTest)))
-    # test1= np.right_shift(valuetoTest, cloud_bit)
-    # print("np.right_shift value ")
-    # print(test1)
-    # print("np.right_shift value binary value")
-    # print(bin(test1))
-    # print("np.bitwise_and(np.right_shift(valuetoTest, cloud_bit), 1) ")
-    # print(np.bitwise_and(test1, 1))
-    # print("binary np.bitwise_and(test1, 1) ")
-    # print(bin(np.bitwise_and(test1, 1)))
-
     # Create cloud mask based on confidence thresholds
     cloud_mask = np.bitwise_and(np.right_shift(qa_bandTest, cloud_bit), 1)
     cloud_shadow_mask = np.bitwise_and(np.right_shift(qa_bandTest, cloud_shadow_bit), 1)
@@ -72,15 +50,7 @@ def cloud_mask_landsat8_clip(image_path, bbcoord):
     # Combine cloud and cloud shadow masks
     
     combined_mask = np.logical_or(cloud_mask, cloud_shadow_mask)
-    # print(combined_mask.shape)      
-    #Plot the cloud mask
-    # Need to squeeze before plotting
 
-    # plt.figure(figsize=(10, 10))
-    # plt.imshow(combined_mask.squeeze(), cmap='gray')
-    # plt.title('Cloud Mask')
-    # plt.colorbar()
-    # plt.show()
     combined_mask =  np.where((combined_mask==0)|(combined_mask==1), combined_mask^1,combined_mask) # Convert zeors to one and ones to zero
     combined_mask = np.where(combined_mask==0, np.nan, combined_mask) # Assign null values to zeros
     return combined_mask,resultTest[1]
--- app/indexcalculator/writeRasterFunction.py
+++ app/indexcalculator/writeRasterFunction.py
@@ -1,10 +1,17 @@
 import rasterio
 import sys
 def writeRaster(out_img,out_meta,out_tif): # Input numpy array of the biophysical output, metadata of clipped raster, output location
+  	# Input: `out_img` - numpy array of the biophysical output
+    #        `out_meta` - metadata of the clipped raster
+    #        `out_tif` - output location to save the raster (GeoTIFF)
+
 	try:
+		# Open a new GeoTIFF file for writing with the provided metadata
 		with rasterio.open(out_tif, "w", **out_meta) as dest:
+			# Write the `out_img` numpy array to the newly created GeoTIFF file
 			dest.write(out_img)
 	except Exception:
+		# Handle any exception that occurs during the writing process
 		print ("Invalid output file location")
 		sys.exit()
 
